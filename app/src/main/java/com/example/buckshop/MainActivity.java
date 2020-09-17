507f7c51f2aaf1c1fefe5ab0d55f927afb3baab9
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-09-17 15:31:09 +0530

    Register Activity & activity_register.xml Working on the design of the XML File & Created the Intent for the main activity to the login activity and to the register activity. Xml file design changed a bit.

--- app/src/main/java/com/example/buckshop/MainActivity.java
+++ app/src/main/java/com/example/buckshop/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.buckshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
@@ -17,5 +19,25 @@ public class MainActivity extends AppCompatActivity
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/buckshop/RegisterActivity.java
+++ app/src/main/java/com/example/buckshop/RegisterActivity.java
@@ -3,12 +3,26 @@ package com.example.buckshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity
+{
+
+    private Button CreateAccountButton;
+    private EditText InputName, InputPhoneNumber, InputPassword;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        CreateAccountButton = (Button) findViewById(R.id.register_btn);
+        InputName = (EditText) findViewById(R.id.register_username_input);
+        InputPhoneNumber = (EditText) findViewById(R.id.register_phone_number_input);
+        InputPassword = (EditText) findViewById(R.id.register_password_input);
+
+
     }
 }
