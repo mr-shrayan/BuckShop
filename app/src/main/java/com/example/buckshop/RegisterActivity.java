69335fd4b98e1a95c488081c681cdce3ef69240d
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-09-20 17:24:52 +0530

    RegisterActivity.java User LOGIN Creating the User Login system for the user to register to the app and then login. PART 2. If checking for the fields are empty or filled added. Loading Bar implemented.

--- app/src/main/java/com/example/buckshop/RegisterActivity.java
+++ app/src/main/java/com/example/buckshop/RegisterActivity.java
@@ -2,16 +2,20 @@ package com.example.buckshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity
 {
 
     private Button CreateAccountButton;
     private EditText InputName, InputPhoneNumber, InputPassword;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,6 +27,7 @@ public class RegisterActivity extends AppCompatActivity
         InputName = (EditText) findViewById(R.id.register_username_input);
         InputPhoneNumber = (EditText) findViewById(R.id.register_phone_number_input);
         InputPassword = (EditText) findViewById(R.id.register_password_input);
+        loadingBar = new ProgressDialog(this);
 
 
         CreateAccountButton.setOnClickListener(new View.OnClickListener() {
@@ -42,5 +47,26 @@ public class RegisterActivity extends AppCompatActivity
         String name = InputName.getText().toString();
         String phone = InputPhoneNumber.getText().toString();
         String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(name))
+        {
+            Toast.makeText(this, "Please Write Your Name", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(phone))
+        {
+            Toast.makeText(this, "Please enter a phone number", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Enter a Password", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Creating Account");
+            loadingBar.setMessage("Please Wait");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+        }
     }
 }
